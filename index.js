f4e1a6e3c865a1064fdd81722956ca6ef81d7f45
vinyborges <viny.bdj@gmail.com>
2023-05-04 04:40:44 -0700

    commit tree 091c5e385d01ef4d463b6644d8ca920d67fa7fa8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// We put as argument an array called "drivers" containing 4 names: cosnt drivers = [name1,name2,name3,name4] it is already declared st indexTest.js
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+const createFareMultiplier = function (fareMultiplier) {
+    return function(value) {
+      return fareMultiplier * value;
+    }
+} 
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers,returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
+
+ 
+
+    
+
+
+
+
